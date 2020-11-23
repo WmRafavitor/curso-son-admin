452e9650437bbd8048b88ef311448124be5507a5
Rafael Vitor <wmrafavitor@gmail.com>
2020-11-23 18:20:41 -0300

    commit inicial

---
+++ templates/admin/pages.tpl.php
@@ -0,0 +1,3 @@
+<h3>Adminstração de páginas</h3>
+
+<a href="/admin">Ir para home</a>
---
+++ templates/site.tpl.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Painel administrativo</title>
+</head>
+<body>
+    <h1>Bem vindo a SON</h1>
+    <?php include $content ?>
+
+    <P><small>2018 - todos os direitos reservados</small></P>
+</body>
+</html>
